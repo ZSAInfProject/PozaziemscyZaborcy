b01d32143a9cd8f32b9c1515bfa764da2d33f008
KyattPL <siema1234@gazeta.pl>
2018-05-15 13:05:13 +0200

    Testing stuff

--- test.py
+++ test.py
@@ -2,10 +2,39 @@ import pygame
 
 pygame.init()
 
-gameDisplay = pygame.display.set_mode((500,500))
+gameDisplay = pygame.display.set_mode((500, 500))
+pygame.display.set_caption('Pozaziemscy zaborcy')
 
-pygame.display.update()
+x = 255
+
+y_rect = 400
+x_rect = 100
+velocity = 0
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            gameExit = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                velocity -= 0.2
+            if event.key == pygame.K_d:
+                velocity += 0.2
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                velocity = 0
+            if event.key == pygame.K_d:
+                velocity = 0
+
+    x_rect += velocity
+
+    gameDisplay.fill((x, x, x))
+    pygame.draw.rect(gameDisplay, (0, 0, 0), [x_rect, y_rect, 10, 10])
+    pygame.display.update()
+
 
 pygame.quit()
-
 quit()
