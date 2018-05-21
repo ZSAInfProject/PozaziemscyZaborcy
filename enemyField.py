11d2100d145df80612e9e1b680e6d0a738c12080
Mendrejk <bastian.lakomy@gmail.com>
2018-05-21 18:42:58 +0200

    Fixed wall collision detection

--- enemyField.py
+++ enemyField.py
@@ -1,5 +1,5 @@
-from pygame import draw  # do usunięcia na koniec
 from math import ceil
+from pygame import draw  # do usunięcia na koniec
 import enemyShip
 
 
@@ -11,6 +11,8 @@ class EnemyField:
     enemyWidth = 0
     enemyHeight = 30
     velocity = 0.65
+    offset_x = 0  # te offsety sa potrzebne globalnie w klasie, bo 2 funkcje z nich korzystaja a srednio mozna podac w argumentach
+    offset_y = 0  # chyba ze ktos ma lepszy pomysl
 
     def __init__(self, screen_x, width):
         self.fieldWidth = screen_x-100
@@ -27,7 +29,6 @@ class EnemyField:
             remainder_x += self.enemyWidth
         else:
             howMany_x = ceil(howMany_x / 2)
-
         if howMany_y % 2 == 0:
             howMany_y /= 2
             remainder_y += self.enemyHeight
@@ -38,12 +39,12 @@ class EnemyField:
 
     def fillWithEnemies(self, entities):
         howMany_x, remainder_x, howMany_y, remainder_y = self.howManyEnemies()
-        offset_x = remainder_x/2
-        offset_y = remainder_y/2
-        fieldOffset_y = self.startY + offset_y
+        self.offset_x = remainder_x/2
+        self.offset_y = remainder_y/2
+        fieldOffset_y = self.startY + self.offset_y
         for _ in range(int(howMany_y)):
-            fieldOffset_x = self.startX + offset_x
-            for adversary in range(howMany_x):
+            fieldOffset_x = self.startX + self.offset_x
+            for _ in range(howMany_x):
                 entities.append(enemyShip.EnemyShip(self.enemyWidth, fieldOffset_x, fieldOffset_y))
                 fieldOffset_x += 2*self.enemyWidth
             fieldOffset_y += 2*self.enemyHeight
@@ -56,7 +57,28 @@ class EnemyField:
         else:
             return True
 
+    def findEntityExtremes(self, entities):
+        min_x, min_y = float('Inf'), float('Inf')
+        max_x, max_y = -float('Inf'), -float('Inf')
+
+        for entity in range(1, len(entities)):
+            if entities[entity].s_x < min_x:
+                min_x = entities[entity].s_x
+            if entities[entity].s_x > max_x:  # musi byc if a nie elif, bo jesli zawsze bedzie true w 1szym, to nie ustawi max_x
+                max_x = entities[entity].s_x
+            if entities[entity].s_y < min_y:
+                min_y = entities[entity].s_y
+            if entities[entity].s_y > max_y:
+                max_y = entities[entity].s_y
+        return min_x, max_x, min_y, max_y
+
     def draw(self, screen_x, gameDisplay, entities):
+        if len(entities) > 1:  # update pola field'a
+            min_x, max_x, min_y, max_y = self.findEntityExtremes(entities)
+            self.startX = min_x - self.offset_x
+            self.fieldWidth = round(max_x - self.startX + entities[1].width) + 2 * self.offset_x
+            self.startY = min_y - self.offset_y
+            self.fieldHeight = round(max_y - self.startY + entities[1].width) + 2 * self.offset_y
         if self.checkWalls(screen_x, gameDisplay):
             self.startX += self.velocity
             for entity in range(1, len(entities)):
