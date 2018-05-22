95a74481cb18b2388069cb869d69874967e53a6d
KyattPL <siema1234@gazeta.pl>
2018-05-22 09:43:02 +0200

    Satisfied PEP8

--- enemyShip.py
+++ enemyShip.py
@@ -10,8 +10,10 @@ class EnemyShip(ship.Ship):
         self.width = given_width
 
     def check_player(self, player):  # wykraczy sie jesli kiedykolwiek gracz/enemy nie bedzie kwadratem
-        if (self.s_x <= player.s_x <= self.s_x + self.width) or (self.s_x <= player.s_x + player.width <= self.s_x + self.width):
-            if (self.s_y <= player.s_y <= self.s_y + self.width) or (self.s_y <= player.s_y + player.width <= self.s_y + self.width):                return True
+        if (self.s_x <= player.s_x <= self.s_x + self.width) or (self.s_x <= player.s_x + player.width <= self.s_x + self.width) and \
+                (self.s_y <= player.s_y <= self.s_y + self.width) or (self.s_y <= player.s_y + player.width <= self.s_y + self.width):
+            return True
+        return False
 
     def shoot(self):
         return bullet.Bullet(self.s_x, -2)
