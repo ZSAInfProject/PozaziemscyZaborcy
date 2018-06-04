95c661a21b090fc99c1d2392308a45231a38d717
Mendrejk <bastian.lakomy@gmail.com>
2018-06-04 13:13:38 +0200

    Added menu shortcuts

--- menu.py
+++ menu.py
@@ -6,31 +6,41 @@ def menu(game_display, GAME):
     menu_exit = False
 
     while not menu_exit:
+
+        key_red, key_green = False, False
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    key_red = True
+                elif event.key == pygame.K_x:
+                    key_red = True
+                elif event.key == pygame.K_z:
+                    key_green = True
 
         game_display.fill((255, 255, 255))
         myfont = pygame.font.SysFont('monospace', 30)
         label = myfont.render("Pozaziemscy zaborcy", 1, (0, 0, 0))
         game_display.blit(label, (70, 150))
-        menu_exit = check_mouse(game_display, GAME, menu_exit)
+        menu_exit = check_mouse(game_display, GAME, menu_exit, key_red, key_green)
 
         pygame.display.update()
         pygame.time.Clock().tick(15)
 
 
-def check_mouse(game_display, GAME, menu_exit):
+def check_mouse(game_display, GAME, menu_exit, key_RED, key_GREEN):
 
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
 
-    if 160 >= mouse[0] >= 60 and 250 <= mouse[1] <= 300:
+    if 160 >= mouse[0] >= 60 and 250 <= mouse[1] <= 300 or key_GREEN:
 
         pygame.draw.rect(game_display, (120, 255, 120), (60, 250, 100, 50))
 
-        if click[0] == 1:
+        if click[0] == 1 or key_GREEN:
             pygame.draw.rect(game_display, (120, 44, 23), (60, 250, 100, 50))
             menu_exit = (0, GAME, menu_exit)
             return menu_exit
@@ -38,11 +48,11 @@ def check_mouse(game_display, GAME, menu_exit):
     else:
         pygame.draw.rect(game_display, (0, 255, 0), (60, 250, 100, 50))
 
-    if 450 >= mouse[0] >= 350 and 250 <= mouse[1] <= 300:
+    if 450 >= mouse[0] >= 350 and 250 <= mouse[1] <= 300 or key_RED:
 
         pygame.draw.rect(game_display, (255, 60, 120), (350, 250, 100, 50))
 
-        if click[0] == 1:
+        if click[0] == 1 or key_RED:
             pygame.draw.rect(game_display, (255, 120, 120), (350, 250, 100, 50))
             menu_exit = check_click(1, GAME, menu_exit)
             return menu_exit
